380c9f558132d7430a8c67404a72c48f5713cd56
expect <a4s8fd96g54@fsd.com>
2023-07-03 14:44:42 +0800

    注释

--- src/main/java/io/github/xuefm/enums/AlignType.java
+++ src/main/java/io/github/xuefm/enums/AlignType.java
@@ -6,6 +6,9 @@ package io.github.xuefm.enums;
 public class AlignType {
 
 
+    /**
+     * 横向对齐方式
+     */
     public enum TransverseAlign {
 
         /**
@@ -26,6 +29,9 @@ public class AlignType {
 
     }
 
+    /**
+     * 纵向对齐方式
+     */
     public enum VerticalAlign {
         /**
          * 顶部对齐
